201195901e409a1b64105826e25abe2156362eb0
ezenity <anthonymmacallister@gmail.com>
2020-03-06 10:28:40 -0500

    Project Mortgage Calculator: Mosh Solution

--- src/com/ezenity/types/Main.java
+++ src/com/ezenity/types/Main.java
@@ -639,6 +639,71 @@ public class Main {
         // Mortgage
         System.out.print("Mortgage: " + projectCurrencyString);
 
+        /*
+         * Project: Mortgage Calculator
+         *
+         * Mosh Example
+         */
+        System.out.println();
+        System.out.println();
+        System.out.println("Mosh Mortgage Calculator");
+        final byte MONTHS_IN_YEARS = 12; // Constant
+        final byte PERCENT = 100; // Constant
+
+        // Scanner scanner = new Scanner(System.in); // Used this exact variable above
+
+        System.out.print("Principal: ");
+        /*
+         * We did not use "short" type because it has a max of 32k and that is not enough. If a person wanted to finance 1 million dollars this would be a problem. Since we used int
+         * we can store a value up to 2 billion.
+         */
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        /*
+         * For the interest rate we could use double or float however, since float uses less memory and is sufficient, it is the better option.
+         */
+        float annualInterest = scanner.nextFloat();
+        /*
+         * Avoid magic numbers when possible so that the next developer can understand what the variables are being used for.
+         *
+         * Magic numbers would be the 12 and 100 in the example below:
+         *      float monthlyInterest = annualInterest / 100 / 12;
+         */
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEARS;
+
+        System.out.print("Period (Years): ");
+        /*
+         * When it comes to how many years a mortgage can be obtained for, byte is the ideal option to use. Byte has a max of 127 which we will never reach will utilize a mortgage period.
+         */
+        byte years = scanner.nextByte();
+        /*
+         * Pretty self explanatory.
+         */
+        int numberOfPayments = years * MONTHS_IN_YEARS;
+
+        /*
+         * Here we will use double since Float is not sufficient. The formal for the equation below is like so:
+         *               r(1 + r)n
+         *      M = P ----------------
+         *              (1 + r)n - 1
+         *
+         *
+         *                              monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)
+         *      Mortgage = Principal ----------------------------------------------------------------------
+         *                                    Math.pow(1 + monthlyInterest, numberOfPayments) -1
+         *
+         * Keep in mind that this program is a simple example and does have a number of problems. The first problem would be input validation. To fix this problem you can use
+         * conditional statements to valid the value the user is inputting.
+         *
+         */
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
+
 
 
 
