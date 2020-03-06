f8b29a15c8e954fb1d5edaca90c9ba33968e2762
ezenity <anthonymmacallister@gmail.com>
2020-03-06 11:02:18 -0500

    Logical Operators

--- src/com/ezenity/controlFlow/Main.java
+++ src/com/ezenity/controlFlow/Main.java
@@ -31,7 +31,29 @@ public class Main {
         int comparisonX = 1;
         int comparisonY = 1;
         System.out.println(comparisonX == comparisonY); // When run, returns true. This is a boolean expression.
-
+        /*
+         * Logical Operators
+         *
+         * Logical operators allows you to combine multiple boolean expressions to get a specific outcome. Keep in mind that when reading expressions it is read from
+         * left to right.
+         *  Operators
+         *      AND: &&
+         *          This operator checks to see if all the expressions are true and will return true, if not will return false
+         *      OR: ||
+         *          This operator checks to see if at least one expression is true. If one expression is true it will return true.
+         */
+        System.out.println();
+        System.out.println("Logical Operators");
+        // AND Operator
+        int logicalAndTemperature = 22;
+        boolean logicalAndIsWarm = logicalAndTemperature > 20 && logicalAndTemperature < 30;
+        System.out.println(logicalAndIsWarm);
+        // OR Operator
+        boolean logicalOrHasHighIncome = true;
+        boolean logicalOrHasGoodCredit = true;
+        boolean logicalOrHasCriminalRecord = false;
+        boolean logicalOrIsEligible = (logicalOrHasHighIncome || logicalOrHasGoodCredit) && !logicalOrHasCriminalRecord;
+        System.out.println(logicalOrIsEligible);
 
     }
 }
