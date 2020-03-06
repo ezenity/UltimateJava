dca3fcada125ef0907c7b9e8e453daa5929df7d1
ezenity <anthonymmacallister@gmail.com>
2020-03-06 10:49:42 -0500

    Comparison Operators

--- src/com/ezenity/controlFlow/Main.java
+++ src/com/ezenity/controlFlow/Main.java
@@ -14,6 +14,24 @@ package com.ezenity.controlFlow;
 public class Main {
 
     public static void main(String[] args){
+        /*
+         * Comparison Operators
+         *
+         * We use these operators to compare primitive values. An example would be: "Are x 7 y equal or not?" To do this we use the equality operator: ==
+         *  Operators
+         *      Equality Operator: ==
+         *      Inequality Operator: !=
+         *      Greater Than: >
+         *      Greater Than or Equal To: >=
+         *      Less Than: <
+         *      Less Than or Equal To: <=
+         */
+        System.out.println();
+        System.out.println("Comparison Operators");
+        int comparisonX = 1;
+        int comparisonY = 1;
+        System.out.println(comparisonX == comparisonY); // When run, returns true. This is a boolean expression.
+
 
     }
 }
