52726934bdfc0a0b2fe7477c856cdc3210f2ec7a
ezenity <anthonymmacallister@gmail.com>
2020-03-09 09:25:55 -0400

    Clean Coding Intro

---
+++ src/com/ezenity/cleanCoding/Main.java
@@ -0,0 +1,17 @@
+package com.ezenity.cleanCoding;
+
+/**
+ * Clean Coding
+ *
+ * "Any fool can write code that computers can understand. Good Programmers write code that humans can understand."
+ * -Martin Fowler
+ *
+ * This section is about how to make your code clean and readable for humans and not just computers. You will also learn
+ * new techniques for changing the structure of the code and making it clean  and beautiful.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
