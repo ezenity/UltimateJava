23483f9afc5fcfdaed2b0121f0f7fec3a74ff581
ezenity <anthonymmacallister@gmail.com>
2020-03-09 09:33:00 -0400

    Clean Coding Intro: More Info

--- src/com/ezenity/cleanCoding/Main.java
+++ src/com/ezenity/cleanCoding/Main.java
@@ -12,6 +12,20 @@ package com.ezenity.cleanCoding;
 public class Main {
 
     public static void main(String[] args){
+        /*
+         * Clean Coding
+         *
+         * As you write code, it is important to keep it clean and organized. For example your house, if it is messy and
+         * full of stuff all over the place you're going to have a difficult time looking for something you're looking for.
+         * However, if your house is clean and neat you will be able to find stuff easily and fast. We use this same concept
+         * in coding as your program grows larger. By breaking down your code into smaller chunks it makes the program
+         * easier to read and understand. Another great use for this is that it can be utilized potentially in other programs.
+         * This also makes your program more maintainable and easier to extend. In the section you will learn some basics
+         * on clean coding and if you would like and in depth course you make check out Mosh's other choruses he has avaialbe
+         * for clean coding.
+         *
+         * In this section we will modify the Mortgage Calculator Project since it is messy and smelly.
+         */
 
     }
 }
