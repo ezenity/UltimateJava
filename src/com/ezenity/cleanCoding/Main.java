692984131be91bedfa7081e38fcc2f32285695a3
ezenity <anthonymmacallister@gmail.com>
2020-03-09 09:50:54 -0400

    Creating Methods

--- src/com/ezenity/cleanCoding/Main.java
+++ src/com/ezenity/cleanCoding/Main.java
@@ -27,5 +27,55 @@ public class Main {
          * In this section we will modify the Mortgage Calculator Project since it is messy and smelly.
          */
 
+        /*
+         * Creating Methods: Sample One
+         *
+         * With the course, the examples Mosh give, he made everything into one method "public static void main(){}" This is okay
+         * for programs that small amounts of code like 10 lines. Anything that is more should be breaking down into different methods
+         * to easily maintain it.
+         */
+        System.out.println("=| Creating Methods |=");
+        greetUserNoValue("Anthony", "MacAllister");
+        /*
+         * Creating Methods: Sample Two
+         *
+         * Now when we call the new method that returns a String we get a new value. all we need to do is simple store it in a variable.
+         */
+        String creatingMethodsMessage = greetUserValue("Anthony", "MacAllister");
+        System.out.println(creatingMethodsMessage);
+
+
+    }
+
+    /*
+     * Creating Methods: Sample One
+     *
+     * When creating a method you start it with an "Access Modifier", Then static or nothing, then return type, then method name, then
+     * parameters() and finally body {}
+     *
+     * Access Modifiers
+     *      public
+     *      private
+     *      protected
+     *
+     * Static: is a method that belongs to a class and not an object.
+     *
+     * Return Type
+     *      void: States that this method does not return values.
+     *
+     */
+    public static void greetUserNoValue(String firstName, String lastName) {
+        System.out.println("Hello " + firstName + " " + lastName);
+
+    }
+
+    /*
+     * Creating Methods: Sample Two
+     *
+     * What if our method returns a value. First change the return type from void to the value type, in our case it is String. Then add a return statement infront
+     * of the String you want to return.
+     */
+    public static String greetUserValue(String firstName, String lastName){
+        return "Hello " + firstName + " " + lastName;
     }
 }
