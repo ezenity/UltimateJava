2519403adcb5376f13a4940690f432f6ed419568
ezenity <anthonymmacallister@gmail.com>
2020-03-10 10:54:08 -0400

    Types Of Errors

--- src/com/ezenity/debuggingDeployingApps/Main.java
+++ src/com/ezenity/debuggingDeployingApps/Main.java
@@ -13,6 +13,15 @@ package com.ezenity.debuggingDeployingApps;
 public class Main {
 
     public static void main(String[] args){
-
+        /*
+         * Types Of Errors
+         *      Compile-time Errors/Syntax Errors: These errors happen when we do not follow the grammar or syntax of Java language
+         *          Easily to find and fix
+         *          Our IDE usually points out these errors
+         *      Run-Time Errors: These errors happen when we run our applications.
+         *          To find these errors we use a tool called a Debugger
+         *
+         * Good source for finding out error information is Google.com & stackoverflow.com
+         */
     }
 }
