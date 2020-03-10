790ffda2b5ed511781bf9bf115d196e575f32982
ezenity <anthonymmacallister@gmail.com>
2020-03-10 11:33:50 -0400

    Packaging Java Applications

--- src/com/ezenity/debuggingDeployingApps/Main.java
+++ src/com/ezenity/debuggingDeployingApps/Main.java
@@ -42,6 +42,43 @@ public class Main {
         System.out.println("Start");
         printNumbers(4);
         System.out.println("Finish");
+        /*
+         * Packaging(Deploy) Java Applications
+         *
+         * The core focus here is deploying console or command line applications. Deploying mobile applications or web applications regard
+         * additional steps that go beyond the scope of this video.
+         *
+         * If you are wanting to give this program to someone else to use you need to package your program into a JAR file.
+         *      JAR = Java Archive
+         *          It is a package format that contains all of your code for distribution.
+         * By having a JAR file you can give it to anyone that has a JRE installed on their computer.
+         *
+         * To create a JAR follow the listed steps below:
+         *      File
+         *      Project Structure...
+         *      Artifacts: An assemble, or a combination of project assets that we put together
+         *          Click "+"
+         *              JAR: Select from modules with dependencies (FOR THIS DEMO)
+         *              Android Application:
+         *              JavaFx Application: JavaFx is a platform for create desktop applications
+         *              JavaFx Pre-loader:
+         *              Other:
+         *      Create JAR from Modules (Window)
+         *          Module: Another level of abstraction above packages
+         *          Main Class: Select Main class
+         *          JAR files from libraries: if this project had other libraries, you could decide what to do with those libraries
+         *
+         * Now that we have a JAR Artifact created we need to build it with the following steps below:
+         *      Build
+         *      Build Artifacts
+         *          Actions:
+         *              "Build" it
+         *              "Rebuild" it: Is the combination of 'Clean' and 'Build'. If something is not working properly, it is good to rebuild an artifact
+         *              "Clean" it: removes all files in the directory that contains this JAR file
+         *
+         *
+         */
+        System.out.println("Hello World");
     }
 
     /*
