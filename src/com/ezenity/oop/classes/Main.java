4574428216206dcbf6e0e1adfbc7862698efdd92
ezenity <anthonymmacallister@gmail.com>
2020-03-11 13:30:40 -0400

    Memory Allocation

--- src/com/ezenity/oop/classes/Main.java
+++ src/com/ezenity/oop/classes/Main.java
@@ -80,5 +80,31 @@ public class Main {
          * However, if you want to see the NPE remove the `= ""` from the 'text' field inside the TextBox{} class.
          */
         System.out.println(textBox3.text.toUpperCase());
+        /*
+         * Memory Allocation
+         *
+         * Java stores two different types of memory:
+         *      Heap
+         *          Store the objects
+         *      Stack
+         *          Stores short-lived variables like primitives and short-lived variables like references to objects on the heap.
+         *
+         * Both of the variables below are referencing the same textBox object on the Heap. We have two variables on the stack however that are
+         * referencing this object. "textBoxMemory1" is the address location on the Heap. So by doing this, if the value is changed it will be
+         * visible to the other variable.
+         *
+         * With Java, we do not need to worry about memory de-allocation, unlike C++. Java automatically removes the memory for us. The way this
+         * works is that when we move out a given method, Java will remove the variables out of the stack it is stored in. However, we still have
+         * objects on the Heap, and with another process when a certain time length occurs Java will remove this object off of the Heap. This is
+         * called "Garbage Collection".
+         *
+         * Garbage Collector: Remove unused objects on the Heap. (The time length can be adjusted using Java runtime parameters.)
+         */
+        System.out.println();
+        System.out.println("Memory Allocation");
+        var textBoxMemory1 = new TextBox();
+        var textBoxMemory2 = textBoxMemory1;
+        textBoxMemory2.setText("Hello World");
+        System.out.println(textBoxMemory1.text);
     }
 }
