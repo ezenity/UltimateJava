491c01fe00e9ecacc86502f70f3303a9ce377f41
ezenity <anthonymmacallister@gmail.com>
2020-03-11 13:42:46 -0400

    Procedural Programming

--- src/com/ezenity/oop/classes/Main.java
+++ src/com/ezenity/oop/classes/Main.java
@@ -106,5 +106,41 @@ public class Main {
         var textBoxMemory2 = textBoxMemory1;
         textBoxMemory2.setText("Hello World");
         System.out.println(textBoxMemory1.text);
+        /*
+         * Procedural Programming
+         *
+         * The below example is what we call procedural programming, and it is not actually Object-oriented programming. Methods that typically
+         * have a lot of parameters are not Object-oriented programming. This makes reusability harder. It is harder to maintain this type of
+         * coding style and you have to go all over the place and look at so much code to figure out what you need to change.
+         *
+         *
+         */
+        System.out.println();
+        System.out.println("Procedural Programming");
+        int baseSalary = 50_000;
+        int extraHours = 10;
+        int hourly = 20;
+
+        int wage = calculateWage(baseSalary,extraHours,hourly);
+        System.out.println(wage);
+
+    }
+
+    /**
+     * Procedural Programming
+     *
+     * Gets the calculating for a wage
+     *
+     * @param baseSalary Get base salary
+     * @param extraHours Get extra worked hours
+     * @param hourlyRate Get hour rate
+     * @return Return total
+     */
+    public static int calculateWage(
+            int baseSalary,
+            int extraHours,
+            int hourlyRate
+    ) {
+        return baseSalary + (extraHours * hourlyRate);
     }
 }
