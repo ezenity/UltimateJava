9c6e60487fdf6e821d6a8f79fcf35a7aaf8b417f
ezenity <anthonymmacallister@gmail.com>
2020-03-11 11:12:59 -0400

    Programming Paradigms

---
+++ src/com/ezenity/oop/Main.java
@@ -0,0 +1,44 @@
+package com.ezenity.oop;
+
+/**
+ * Object-oriented Programming
+ *
+ *
+ */
+public class Main {
+    public static void main(String[] args){
+        /*
+         * Programming Paradigms
+         *  In computer Science you have varies programming paradigms(styles of programming), like:
+         *      Procedural
+         *      Functional (Most Popular)
+         *      Object-oriented (Most Popular)
+         *          Everything is based on the concept of objects. these objects contain some data (also called state)
+         *          and operations on the data (Also called behaviour). These are the methods or functions that
+         *          modify the data.
+         *      Event-driven
+         *      Logic
+         *      Aspect-oriented
+         *
+         * Not all programmable languages support multiple paradigms. A program language called "SmallTalk (OOP)" only
+         * supports a single paradigm. While other languages support multiple paradigms, such as:
+         *      Python
+         *      Ruby
+         *      Java
+         *      JavaScript
+         *
+         * In object-oriented programming, you bring together the data and methods that operate on a single object. Check
+         * out the diagram example below
+         *       =] Object-oriented [=
+         *              Object
+         *               Data
+         *             Behaviour
+         *
+         * In functional programming you take the opposite approach. We assume that the data and behavior are fundamentally
+         * different, so we keep them separated. See the example below:
+         *       =] Functional [=
+         *        Data    Behaviour
+         */
+
+    }
+}
