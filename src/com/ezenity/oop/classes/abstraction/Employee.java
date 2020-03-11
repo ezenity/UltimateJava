d596066b83da06a34ba5a348462216d7075eeb2a
ezenity <anthonymmacallister@gmail.com>
2020-03-11 14:36:04 -0400

    Abstraction

---
+++ src/com/ezenity/oop/classes/abstraction/Employee.java
@@ -0,0 +1,40 @@
+package com.ezenity.oop.classes.abstraction;
+
+/**
+ * This would be the implementation detail. We do not want to directly work with this class.
+ */
+public class Employee {
+    private int baseSalary;
+    private int hourlyRate;
+    // public int extraHours;
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0)
+            throw new IllegalArgumentException("Please add a hourly rate above 0.");
+
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0)
+         throw new IllegalArgumentException("Please add a base salary above 0.");
+
+        this.baseSalary = baseSalary;
+    }
+
+    /*
+     * Let's make an assumption. Let's say that the baseSalary & hourlyRate are fixed for one year but, the extraHours vary
+     * depending on the month. So instead we want to pass it as an argument to this method.
+     */
+    public int calculateWage(int extraHours){
+        return baseSalary + (hourlyRate * extraHours);
+    }
+}
---
+++ src/com/ezenity/oop/classes/abstraction/Main.java
@@ -0,0 +1,28 @@
+package com.ezenity.oop.classes.abstraction;
+
+/**
+ * Abstraction
+ *
+ * Reduce complexity by hiding unnecessary details in our classes
+ *
+ * Metaphor
+ *
+ * A TV Remote
+ *  Inside the TV remote there is a mother board with a lot of complex details however we do not
+ *  interact with this motherboard. It has so much complexity there we don't really understand that and don't want to. We
+ *  just want to change the channel, change the volume and so on. This is what we call abstraction. So with abstraction
+ *  we want to hide the implementation detail of the class and test it like a black box. We don't care what is inside,
+ *  we just care about the simple interface that we can work with, like the buttons on the remote.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println();
+        System.out.println("Getters & Setters");
+        var employee = new Employee();
+        // employee.setBaseSalary(-1);
+        employee.setBaseSalary(50_000);
+        employee.setHourlyRate(20);
+        int wage = employee.calculateWage(10);
+        System.out.println(wage);
+    }
+}
