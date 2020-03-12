e27ac4684ce922af840d6b69dcecc0a53782cf46
ezenity <anthonymmacallister@gmail.com>
2020-03-12 14:04:13 -0400

    Making console use method overloading

--- src/com/ezenity/oop/refactoringObjectOrientedDesign/theSolution/Console.java
+++ src/com/ezenity/oop/refactoringObjectOrientedDesign/theSolution/Console.java
@@ -2,7 +2,15 @@ package com.ezenity.oop.refactoringObjectOrientedDesign.theSolution;
 
 import java.util.Scanner;
 
+/**
+ * Since there is only going to be one console we have these methods as static.
+ */
 public class Console {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static double readNumber(String prompt){
+        return scanner.nextDouble();
+    }
     /**
      * This method is a generic while loop for gathering a min and max number with a custom string output
      *
@@ -12,7 +20,6 @@ public class Console {
      * @return Return user value input
      */
     public static double readNumber(String prompt, double min, double max){
-        Scanner scanner = new Scanner(System.in);
         double value;
 
         while (true) {
