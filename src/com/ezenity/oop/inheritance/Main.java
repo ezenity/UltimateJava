62886b9bafe503982c6eb733b119d908d7fdb1e2
ezenity <anthonymmacallister@gmail.com>
2020-03-20 19:40:47 -0400

    Inheritance Intro

---
+++ src/com/ezenity/oop/inheritance/Main.java
@@ -0,0 +1,32 @@
+package com.ezenity.oop.inheritance;
+
+/**
+ * Inheritance
+ * <p>
+ * Third principle in Object-oriented Programming.
+ * <p>
+ * This section will cover Constructors, Access Modifiers, Overriding Methods, Comparing Objects,
+ * and Polymorphism.
+ *
+ * We use inheritance where we want to consolidate reused code in one location. We will basically create
+ * a single class that will have these behaviours where other classes will inherit these controls.
+ *
+ * This single class can be called any of the following:
+ * <lu>
+ *     <li>Base</li>
+ *     <li>Super</li>
+ *     <li>Parent</li>
+ * </lu>
+ * Where the class that will inherit this single class could be called any of the following:
+ * <ul>
+ *     <li>Sub</li>
+ *     <li>Child</li>
+ * </ul>
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
