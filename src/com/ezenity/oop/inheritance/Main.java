a2b17f357dd875e248581603d25a6ca99b5d950d
ezenity <anthonymmacallister@gmail.com>
2020-03-20 20:31:16 -0400

    Upcasting & Downcasting

--- src/com/ezenity/oop/inheritance/Main.java
+++ src/com/ezenity/oop/inheritance/Main.java
@@ -23,6 +23,12 @@ package com.ezenity.oop.inheritance;
  *     <li>Child</li>
  * </ul>
  *
+ * <h3>Upcasting</h3>
+ * Casting an object to one of its <b>super</b> types
+ *
+ * <h3>Downcasting</h3>
+ * Casting an object to one of its <b>sub</b> types
+ *
  */
 public class Main {
 
@@ -37,5 +43,25 @@ public class Main {
         //System.out.println(box2.hashCode());
         box2.setText("Hello World");
         System.out.println(box1);
+
+        System.out.println();
+        System.out.println();
+
+        //var controlCasting = new UIControl(true);
+        //show(controlCasting);
+
+        //System.out.println();
+
+        var textBoxControl = new TextBox();
+        show(textBoxControl);
+
+    }
+
+    public static void show(UIControl uiControl){
+        if (uiControl instanceof TextBox) {
+            var textBox = (TextBox) uiControl; // downcasting
+            textBox.setText("Hello World");
+        }
+        System.out.println(uiControl);
     }
 }
