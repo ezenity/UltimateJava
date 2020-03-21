58d4a589b8e8db7619b9dd471ad9c50faeb2b9bc
ezenity <anthonymmacallister@gmail.com>
2020-03-21 09:56:36 -0400

    Intro to Interfaces

---
+++ src/com/ezenity/oop/interfaces/Main.java
@@ -0,0 +1,33 @@
+package com.ezenity.oop.interfaces;
+
+/**
+ * Interfaces
+ *
+ * This section talks about:
+ * <ul>
+ *     <li>What interfaces are?</li>
+ *     <ul>
+ *         <li>
+ *             We use interfaces to build loosely-coupled, extensible, testable applications. Some examples are:
+ *             <ul>
+ *                 <li>Data Compression</li>
+ *                 <li>Encryption</li>
+ *                 <li>Sorting</li>
+ *                 <li>Searching</li>
+ *             </ul>
+ *         </li>
+ *         <li>
+ *             Contains method declarations with no implementations (has no code)
+ *         </li>
+ *     </ul>
+ *     <li>Why we need interfaces</li>
+ *     <li>How to use interfaces "properly"</li>
+ *     <li>Dependency Injection</li>
+ * </ul>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
